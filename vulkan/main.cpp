d36633e8d016331817e7d5c3c8124a567ceace98
cothao <collinthao685@gmail.com>
2025-12-24 22:10:48 -0600

    finished 1st tutortial

--- vulkan/main.cpp
+++ vulkan/main.cpp
@@ -1,12 +1,44 @@
 #include <iostream>
 #include <glm.hpp>
 
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define WINDOW_WIDTH 1920
+#define WINDOW_HEIGHT 1080
+
+GLFWwindow* window = NULL;
+
+void GLFW_KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if ((key == GLFW_KEY_ESCAPE) && (action == GLFW_PRESS)) glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
 int main()
 {
 
-	glm::vec3 test = glm::vec3(1);
+	if (!glfwInit()) return 1;
+	
+	if (!glfwVulkanSupported()) return 1;
 
-	std::cout << "Hello World!\n";
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Tutorial 01", NULL, NULL);
+
+	if (!window)
+	{
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+
+	glfwSetKeyCallback(window, GLFW_KeyCallback);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
 
 	return 0;
 
